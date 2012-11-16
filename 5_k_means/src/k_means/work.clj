029dea4138637955117a59c67fa4c33bf0ca007d
Zayankovsky <zayankovsky@gmail.com>
2012-11-16 22:34:30 +0300

    Solution to task #5

--- 5_k_means/src/k_means/work.clj
+++ 5_k_means/src/k_means/work.clj
@@ -14,17 +14,48 @@
 
 ; (run-empty SOLUTION)
 
-; (run-2-circles SOLUTION)
+(defn distance [[x1 y1] [x2 y2]]
+  (let [dx (- x1 x2)
+        dy (- y1 y2)]
+    (Math/sqrt (+ (* dx dx) (* dy dy)))))
 
-; (run-3-circles SOLUTION)
+(defn mean [points]
+  (map #(/ % (count points)) (apply map + points)))
+
+(defn run-2-circles-solution [points]
+  ((fn lloyd [center1 center2]
+    (let [parts (reduce #(if (<= (distance %2 center1) (distance %2 center2)) [(conj (first %1) %2) (last %1)] [(first %1) (conj (last %1) %2)]) [[] []] points)
+          new-center1 (mean (first parts))
+          new-center2 (mean (last parts))]
+      (if (and (= center1 new-center1) (= center2 new-center2)) parts (lloyd new-center1 new-center2)))) (first points) (last points)))
+
+;(run-2-circles run-2-circles-solution)
+
+(defn run-3-circles-solution [points]
+  ((fn lloyd [center1 center2 center3]
+    (let [parts (reduce #(let [d1 (distance %2 center1)
+                               d2 (distance %2 center2)
+                               d3 (distance %2 center3)]
+                           (if (<= d1 d2)
+                             (if (<= d1 d3) [(conj (first %1) %2) (second %1) (last %1)]
+                                            [(first %1) (second %1) (conj (last %1) %2)])
+                             (if (<= d2 d3) [(first %1) (conj (second %1) %2) (last %1)]
+                                            [(first %1) (second %1) (conj (last %1) %2)]))) [[] [] []] points)
+          new-center1 (mean (first parts))
+          new-center2 (mean (second parts))
+          new-center3 (mean (last parts))]
+      (if (and (= center1 new-center1) (= center2 new-center2) (= center3 new-center3)) parts (lloyd new-center1 new-center2 new-center3)))) (first points) (nth points (/ (count points) 2)) (last points)))
+
+(run-3-circles run-3-circles-solution)
 
 ;;; Manipulation: mouse click - add new point
-;;;               space - reset simulation (remove all points or regenerate them, depenends on test)
+;;;               space - reset simulation (remove all points or regenerate them, depends on test)
 ;;; Note that may need use different solutions (with k = 2 for run-2-circles and  k = 3 for run-3-circles).
 
 
 
 ;;; Now try to improve your solution so it can determine k based on given points. So if there are visually 3 clusters it should partition points to 3 clusters, if 4 than to 4 clusters.
+;;; I have no idea how to do it, excluding some unrealizable ones from here: http://en.wikipedia.org/wiki/Determining_the_number_of_clusters_in_a_data_set
 ;;; Test your solution on this test:
 
 ; (run-random-circles SOLUTION)
